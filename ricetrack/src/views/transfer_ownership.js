f4eb4b7014174c671ad624e68b97054d22dac1a4
zuzen <34233702+zuzen@users.noreply.github.com>
2023-12-23 08:30:56 +0000

    move away transfer ownership from rice_details.js

--- ricetrack/src/views/transfer_ownership.js
+++ ricetrack/src/views/transfer_ownership.js
@@ -1,15 +1,20 @@
 'use strict'
 
 const m = require('mithril');
+const moment = require('moment');
 const payloads = require('../services/payloads');
 const transactions = require('../services/transactions');
 const api = require('../services/api');
+const parsing = require('../services/parsing');
+const {getPropertyValue} = require('../utils/records')
 
 const TransferOwnership = {
     oninit: (vnode) => {
         vnode.state.record = null;
         vnode.state.agents = [];
         vnode.state.selectedAgent = null;
+        vnode.state.tgltransaksi = moment().format('YYYY-MM-DDTHH:mm');
+        vnode.state.harga = '';
 
         // Load the record and agents
         Promise.all([
@@ -17,39 +22,73 @@ const TransferOwnership = {
             api.get('agents')
         ]).then(([record, agents]) => {
             vnode.state.record = record;
-            vnode.state.agents = agents;
+            vnode.state.agents = agents.filter(agent => agent.key !== record.owner);
+            vnode.state.harga = parseInt(getPropertyValue(record, 'harga', 0)).toLocaleString('id');
         });
     },
 
     view: (vnode) => {
         return m('.transfer-ownership',
-            m('h2', 'Transfer Ownership'),
-            m('select', {
-                onchange: m.withAttr('value', (value) => {
-                    vnode.state.selectedAgent = value;
+            m('h2', `Penjualan ${vnode.attrs.recordId}`),
+            m('.form-group',
+                m('label', 'Tanggal Transaksi:'),
+                m('input.form-control', {
+                    type: 'datetime-local',
+                    value: vnode.state.tgltransaksi,
+                    onchange: m.withAttr('value', value => {
+                        vnode.state.tgltransaksi = value;
+                    })
                 })
-            },
-            vnode.state.agents.map(agent =>
-                m('option', { value: agent.key }, agent.name)
-            )),
-            m('button', {
-                onclick: () => _submitTransfer(vnode)
-            }, 'Transfer')
+            ),
+            m('.form-group',
+                m('label', 'Pilih Pembeli:'),
+                m('select.form-control', {
+                    onchange: m.withAttr('value', (value) => {
+                        vnode.state.selectedAgent = value;
+                    })
+                }, vnode.state.agents.map(agent =>
+                    m('option', { value: agent.key }, agent.name)
+                ))
+            ),
+            m('.form-group',
+                m('label', 'Harga (Rp):'),
+                m('input.form-control', {
+                    type: 'text',
+                    value: vnode.state.harga,
+                    onchange: m.withAttr('value', value => {
+                        vnode.state.harga = value;
+                    })
+                })
+            ),
+            m('.form-group',
+                m('button.btn.btn-primary', {
+                    onclick: () => _submitTransfer(vnode)
+                }, 'Jual')
+            )
         );
     }
 }
 
 const _submitTransfer = (vnode) => {
+    const timestamp = new Date(vnode.state.tgltransaksi).getTime();
+
+    const harga = parsing.toInt(vnode.state.harga.replace(/[^0-9]/g, ''));
+
     const transferPayload = payloads.createProposal({
         recordId: vnode.state.record.recordId,
         receivingAgent: vnode.state.selectedAgent,
-        role: payloads.createProposal.enum.OWNER
+        role: payloads.createProposal.enum.OWNER,
+        properties: [
+            { name: 'tgltransaksi', intValue: timestamp },
+            { name: 'harga', intValue: harga }
+        ]
     });
 
     transactions.submit([transferPayload], true)
         .then(() => {
             console.log('Successfully submitted transfer proposal');
-            m.route.set('/'); // Redirect to home or another page after submission
+            alert('Penjualan sudah dilakukan. Menunggu konfirmasi dari pembeli.');
+            m.route.set('/'); // Redirect after showing the message
         });
 }
 

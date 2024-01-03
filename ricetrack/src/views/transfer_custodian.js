9b0b78c14ef69cefc10337701866ea1d4b838f0a
salfath <63273039+salfath@users.noreply.github.com>
2024-01-03 06:52:07 +0000

    Cek apakah ada proposal kustodian yang perlu dicancel Transfer Custodian berjalan smooth

--- ricetrack/src/views/transfer_custodian.js
+++ ricetrack/src/views/transfer_custodian.js
@@ -6,65 +6,114 @@ const payloads = require('../services/payloads');
 const transactions = require('../services/transactions');
 const api = require('../services/api');
 const { _agentByKey } = require('./recordUtils')
+const { _answerProposal, ROLE_TO_ENUM } = require('./proposalUtils');
+
 
 const TransferCustodian = {
-    oninit: (vnode) => {
+    oninit: async (vnode) => {
         vnode.state.record = null;
         vnode.state.agents = [];
         vnode.state.selectedAgent = null;
         vnode.state.custodian = '';
+        vnode.state.proposals = [];
         vnode.state.tgltransaksi = moment().format('YYYY-MM-DDTHH:mm');
+        vnode.state.publicKey = api.getPublicKey();
 
+        await _loadData(vnode.attrs.recordId, vnode.state);
 
-        // Load the record and agents
-        Promise.all([
-            api.get(`records/${vnode.attrs.recordId}`),
-            api.get('agents')
-        ]).then(([record, agents]) => {
-            vnode.state.record = record;
-            vnode.state.agents = agents.filter(agent => agent.key !== record.custodian);
-            vnode.state.selectedAgent = vnode.state.agents[0].key;
-            vnode.state.custodian = _agentByKey(agents, record.custodian).name;
-        });
+        vnode.state.refreshId = setInterval( async () => {
+            await _loadData(vnode.attrs.recordId, vnode.state);
+        }, 60000);
+        vnode.state.selectedAgent = vnode.state.agents[0].key;
     },
 
     view: (vnode) => {
+        console.log('Record: ', vnode.state.record);
+        console.log('Proposals: ', vnode.state.proposals);
+        console.log('Record ID: ', vnode.attrs.recordId);
 
+        const publicKey = vnode.state.publicKey;
+        console.log('Public Key: ', publicKey);
+        let proposalsToAnswer = vnode.state.proposals.filter(proposal =>
+            (proposal.issuingAgent === publicKey) && (proposal.role.toLowerCase() === 'custodian'));
+        let showNewCustodianForm = proposalsToAnswer.length === 0;
+        console.log('Proposals to answer: ', proposalsToAnswer);
 
         return m('.transfer-custodian',
-            m('h2', `Ubah Kustodian`),
-            m('h2', { style: { 'font-size': 'smaller' } }, `${vnode.attrs.recordId}`),
-            m('label', 'Kustodian Saat Ini: ' + vnode.state.custodian),
-            m('.form-group',
-                m('label', 'Tanggal Transaksi:'),
-                m('input.form-control', {
-                    type: 'datetime-local',
-                    value: vnode.state.tgltransaksi,
-                    onchange: m.withAttr('value', value => {
-                        vnode.state.tgltransaksi = value;
+            proposalsToAnswer.length > 0
+                ? proposalsToAnswer.map(proposal =>
+                    m('.proposal-to-answer',
+                        m('p', `Anda sudah meminta ${_agentByKey(vnode.state.agents, proposal.receivingAgent).name} menjadi Kustodian produk ini.`),
+                        m('button.btn.btn-danger', {
+                            onclick: async () => {
+                                await _answerProposal(vnode.state.record, proposal.receivingAgent, ROLE_TO_ENUM[proposal.role.toLowerCase()], payloads.answerProposal.enum.CANCEL)
+                                    .then( async () => {
+                                        return await _loadData(vnode.attrs.recordId, vnode.state); // Memuat data terbaru
+                                    })
+                                    .then(() => {
+                                        m.redraw(); // Memperbarui UI setelah data dimuat
+                                    })
+                                    .catch(err => {
+                                        console.error('Error while answering proposal:', err);
+                                        // Handle error jika diperlukan
+                                    });
+                            }
+                        }, 'Batalkan'),
+                        m('button.btn.btn-primary', {
+                            onclick: () => {
+                                window.history.back();
+                            }
+                        }, 'Kembali')
+                    )
+                )
+                : null,
+            showNewCustodianForm ? [
+                m('h2', `Ubah Kustodian`),
+                m('h2', { style: { 'font-size': 'smaller' } }, `${vnode.attrs.recordId}`),
+                m('label', 'Kustodian Saat Ini: ' + vnode.state.custodian),
+                m('.form-group',
+                    m('label', 'Tanggal Transaksi:'),
+                    m('input.form-control', {
+                        type: 'datetime-local',
+                        value: vnode.state.tgltransaksi,
+                        onchange: m.withAttr('value', value => {
+                            vnode.state.tgltransaksi = value;
+                        })
                     })
-                })
-            ),
-            m('.form-group',
-                m('label', 'Pilih Kustodian Baru: '),
-                m('select.form-control', {
-                    onchange: m.withAttr('value', (value) => {
-                        vnode.state.selectedAgent = value;
-                    })
-                }, vnode.state.agents.map(agent =>
-                    m('option', { value: agent.key }, agent.name)
-                ))
-            ),
-            m('.form-group',
-                m('button.btn.btn-primary', {
-                    onclick: () => _submitTransfer(vnode)
-                }, 'Ubah')
-            )
+                ),
+                m('.form-group',
+                    m('label', 'Pilih Kustodian Baru: '),
+                    m('select.form-control', {
+                        onchange: m.withAttr('value', (value) => {
+                            vnode.state.selectedAgent = value;
+                        })
+                    }, vnode.state.agents.map(agent =>
+                        m('option', { value: agent.key }, agent.name)
+                    ))
+                ),
+                m('.form-group',
+                    m('button.btn.btn-primary', {
+                        onclick: async () => {
+                            await _submitTransfer(vnode)
+                            .then( async () => {
+                                return await _loadData(vnode.attrs.recordId, vnode.state); // Memuat data terbaru
+                            })
+                            .then(() => {
+                                m.redraw(); // Memperbarui UI setelah data dimuat
+                            })
+                            .catch(err => {
+                                console.error('Error while answering proposal:', err);
+                                // Handle error jika diperlukan
+                            });
+                        }
+                    }, 'Ubah')
+                )
+            ] : null
         );
     }
 }
 
-const _submitTransfer = (vnode) => {
+const _submitTransfer = async (vnode) => {
     const timestamp = new Date(vnode.state.tgltransaksi).getTime();
 
     const transferPayload = payloads.createProposal({
@@ -74,16 +123,25 @@ const _submitTransfer = (vnode) => {
         properties: [{ name: 'tgltransaksi', intValue: timestamp },]
     });
 
-    transactions.submit([transferPayload], true)
-        .then(() => {
-            console.log('Successfully submitted transfer proposal');
-            alert('Pengajuan ubah kustodian sudah selesai dilakukan. Menunggu konfirmasi.');
-            m.route.set('/'); // Redirect after showing the message
-        })
-        .catch(err => {
-            console.error('Failed to submit custodian transfer proposal:', err)
-            alert('Failed to submit custodian transfer proposal.')
-        })
-}
+    try {
+        await transactions.submit([transferPayload], true);
+        console.log('Successfully submitted transfer proposal');
+        alert('Pengajuan ubah kustodian sudah selesai dilakukan. Menunggu konfirmasi.');
+        window.history.back();
+    } catch (err) {
+        console.error('Failed to submit custodian transfer proposal:', err);
+        alert('Failed to submit custodian transfer proposal.');
+    }
+};
+
+const _loadData = async (recordId, state) => {
+    const [record, agents] = await Promise.all([api.get(`records/${recordId}`), api.get('agents')]);
+    state.record = record;
+    state.proposals = record.proposals;
+    state.agents = agents.filter(agent => agent.key !== record.custodian);
+    state.custodian = _agentByKey(agents, record.custodian).name;
+
+    m.redraw();
+};
 
 module.exports = TransferCustodian;

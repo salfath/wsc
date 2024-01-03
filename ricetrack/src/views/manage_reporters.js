e8cf2e8eac9cc21aa24c48761564d7544439ec65
salfath <63273039+salfath@users.noreply.github.com>
2024-01-03 07:56:00 +0000

    menambah cek proposal reporter yang diajukan oleh pengguna

--- ricetrack/src/views/manage_reporters.js
+++ ricetrack/src/views/manage_reporters.js
@@ -2,8 +2,10 @@
 
 const m = require('mithril');
 const api = require('../services/api');
+const payloads = require('../services/payloads');
 const { _revokeAuthorization, _authorizeReporter, _reporters } = require('./reporterUtils');
 const { _agentByKey } = require('./recordUtils')
+const { _answerProposal, ROLE_TO_ENUM } = require('./proposalUtils');
 
 const _loadData = async (recordId, state) => {
   try {
@@ -14,6 +16,8 @@ const _loadData = async (recordId, state) => {
     state.agents = agents;
     state.currentReporters = getCurrentReporters(record);
     state.potentialReporters = getPotentialReporters(agents, record, state.currentReporters);
+    state.proposals = record.proposals;
+
     // Set selectedReporterKey to the first potential reporter after data load
     if (state.potentialReporters.length > 0) {
       state.selectedReporterKey = state.potentialReporters[0].key;
@@ -54,14 +58,14 @@ const getPotentialReporters = (agents, record, currentReporters) => {
   );
 };
 
-
-
 const ManageReporters = {
   async oninit(vnode) {
     vnode.state.recordId = vnode.attrs.recordId;
     vnode.state.currentReporters = [];
     vnode.state.potentialReporters = [];
     vnode.state.selectedReporterKey = null;
+    vnode.state.proposals = [];
+    vnode.state.publicKey = api.getPublicKey();
 
     await _loadData(vnode.attrs.recordId, vnode.state)
     vnode.state.refreshId = setInterval(async () => {
@@ -79,21 +83,43 @@ const ManageReporters = {
     }
 
     const { recordId, agents, potentialReporters, currentReporters, selectedReporterKey } = vnode.state;
-    const selectedProperty = ['lokasi'];
+    const publicKey = vnode.state.publicKey;
+    let proposalsToAnswer = vnode.state.proposals.filter(proposal =>
+      proposal.issuingAgent === publicKey && proposal.role.toLowerCase() === 'reporter');
 
-    console.log('currentReporters:', currentReporters);
-    console.log('potentialReporters:', potentialReporters);
+    let showManageReporterForm = proposalsToAnswer.length === 0;
 
-    return m('.manage-reporters',
-      [
-        m('h1', 'Kelola Reporter'), // Main title
+    return m('.manage-reporters', [
+      // Display proposals to answer
+      proposalsToAnswer.length > 0 && proposalsToAnswer.map(proposal =>
+        m('.proposal-to-answer',
+          m('p', `Anda sudah meminta ${_agentByKey(agents, proposal.receivingAgent).name} menjadi Reporter produk ini.`),
+          m('button.btn.btn-danger', {
+            onclick: async () => {
+              await _answerProposal(vnode.state.record, proposal.receivingAgent, ROLE_TO_ENUM[proposal.role.toLowerCase()], payloads.answerProposal.enum.CANCEL)
+                .then(async () => await _loadData(vnode.attrs.recordId, vnode.state))
+                .then(() => m.redraw())
+                .catch(err => console.error('Error while answering proposal:', err));
+            }
+          }, 'Batalkan')
+        )
+      ),
+
+      proposalsToAnswer.length > 0 && m('button.btn.btn-primary', {
+        onclick: () => window.history.back()
+      }, 'Kembali'),
+
+      // Manage reporter form
+      showManageReporterForm && [
+        m('h1', 'Kelola Reporter'),
         m('h2', { style: { 'font-size': 'smaller' } }, `Record ID: ${recordId}`),
         m('h4', 'Current Reporters:'),
+        // Current reporters
         currentReporters.map(([key, properties]) =>
           m('div.reporter-item', { style: { display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px' } }, [
-            m('h6', { style: { marginRight: '10px' } }, `${_agentByKey(vnode.state.agents, key).name}`),
+            m('h6', { style: { marginRight: '10px' } }, `${_agentByKey(agents, key).name}`),
             m('button.btn.btn-primary', {
-              onclick: async (e) => {
+              onclick: async () => {
                 if (window.confirm('Apakah Anda yakin ingin mencabut otorisasi reporter ini?')) {
                   await _revokeAuthorization(recordId, key, properties);
                   await _loadData(vnode.attrs.recordId, vnode.state);
@@ -101,43 +127,34 @@ const ManageReporters = {
               }
             }, 'Hapus')
           ])
-        )],
-      m('h4', 'Add Reporters:'),
-      potentialReporters.length > 0 && m('div',
-        [
-          m('select', {
-            onchange: m.withAttr('value', value => vnode.state.selectedReporterKey = value),
-            value: selectedReporterKey
-          },
-            potentialReporters.map(agent =>
-              m('option', { value: agent.key }, agent.name)
-            )
-          ),
-          m('div', { style: { display: 'flex', marginBottom: '5px' } }),
-          m('button.btn.btn-primary', {
-            onclick: async () => {
-              // Pastikan reporterKey telah dipilih
-              if (selectedReporterKey) {
-                await _authorizeReporter(recordId, selectedReporterKey, selectedProperty)
-                  .then(() => {
-                    alert("Proposal menjadi reporter telah di kirim ke " +
-                      _agentByKey(agents, selectedReporterKey).name + ".")
-                  })
-                  .catch(error => {
-                    // Tampilkan kesalahan jika ada
-                    alert("Failed to send proposal: " + error.message);
-                  });
-                await _loadData(vnode.attrs.recordId, vnode.state);
-              } else {
-                // Tampilkan pesan kesalahan jika reporter belum dipilih
-                alert("Please select a reporter to authorize.");
+        ),
+        m('h4', 'Add Reporters:'),
+        // Potential reporters
+        potentialReporters.length > 0 && m('div',
+          [
+            m('select', {
+              onchange: m.withAttr('value', value => vnode.state.selectedReporterKey = value),
+              value: selectedReporterKey
+            }, potentialReporters.map(agent => m('option', { value: agent.key }, agent.name))),
+            m('button.btn.btn-primary', {
+              onclick: async () => {
+                if (selectedReporterKey) {
+                  await _authorizeReporter(recordId, selectedReporterKey, ['lokasi'])
+                    .then(() => {
+                      alert("Proposal menjadi reporter telah di kirim ke " + _agentByKey(agents, selectedReporterKey).name + ".")
+                      window.history.back();
+                    })
+                    .catch(error => alert("Failed to send proposal: " + error.message));
+                  // await _loadData(vnode.attrs.recordId, vnode.state);
+                } else {
+                  alert("Please select a reporter to authorize.");
+                }
               }
-            }
-          }, 'Authorize')
-        ]
-      )
-
-    );
+            }, 'Authorize')
+          ]
+        )
+      ]
+    ]);
   }
 }
 

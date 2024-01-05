e2ebdbcba1dc32dd3e367116dc8fea82e15f7c70
salfath <63273039+salfath@users.noreply.github.com>
2024-01-05 04:12:58 +0000

    remove transaction date and add reporter

--- ricetrack/src/views/add_rice.js
+++ ricetrack/src/views/add_rice.js
@@ -22,42 +22,24 @@ const varietasOptions = ['IR/Ciherang/Impari', 'Muncul', 'Mentik Wangi', 'IR42',
  * The Form for tracking a new rice.
  */
 const AddRice = {
-  oninit (vnode) {
-  
-    // Format current date and time in a "DD-MM-YYYY" HH:mm format
-    const now = new Date()
-    const day = String(now.getDate()).padStart(2, '0')
-    const month = String(now.getMonth() + 1).padStart(2, '0') // Bulan dimulai dari 0
-    const year = now.getFullYear()
-    const hours = String(now.getHours()).padStart(2, '0')
-    const minutes = String(now.getMinutes()).padStart(2, '0')
-
-    vnode.state.tgltransaksi = `${day}-${month}-${year} ${hours}:${minutes}`
-
+  async oninit (vnode) {
     // Initialize Latitude and Longitude
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
-        vnode.state.latitude = position.coords.latitude || ''
-        vnode.state.longitude = position.coords.longitude || ''
+        vnode.state.latitude = position.coords.latitude;
+        vnode.state.longitude = position.coords.longitude;
+        m.redraw(); // Memaksa update pada komponen setelah mendapatkan lokasi
       }, () => {
-        vnode.state.latitude = ''
-        vnode.state.longitude = ''
-        console.error("Geolocation error or permission denied")
-      })
+        console.error("Geolocation error or permission denied");
+        // Handle error atau kasus ketika izin tidak diberikan
+        m.redraw(); // Memaksa update pada komponen jika ada error
+      });
     } else {
-      console.error("Geolocation is not supported by this browser")
-      vnode.state.latitude = ''
-      vnode.state.longitude = ''
+      console.error("Geolocation is not supported by this browser");
+      // Handle kasus ketika geolocation tidak didukung
+      m.redraw(); // Memaksa update pada komponen jika geolocation tidak didukung
     }
-    
-    // Initialize the empty reporters fields
-    vnode.state.reporters = [
-      {
-        reporterKey: '',
-        properties: []
-      }
-    ]
-    api.get('agents')
+    await api.get('agents')
       .then(agents => {
         const publicKey = api.getPublicKey()
         vnode.state.agents = agents.filter(agent => agent.key !== publicKey)
@@ -96,15 +78,7 @@ const AddRice = {
               ])
             ),
 
-             layout.row([
-               _formGroup('Tanggal Produksi', m('input.form-control', {
-                 type: 'text',
-                 placeholder: 'DD-MM-YYYY HH:mm',                              
-                 oninput: m.withAttr('value', (value) => {
-                   vnode.state.tgltransaksi = value
-                 }),
-                 value: vnode.state.tgltransaksi
-               })),
+            layout.row([
                _formGroup('Berat (kg)', m('input.form-control', {
                  type: 'number',
                  step: 'any',
@@ -112,8 +86,7 @@ const AddRice = {
                    vnode.state.berat = value
                  }),
                  value: vnode.state.berat
-               }))
-             ]),
+               })),
              
              _formGroup('Harga (Rp)', m('input.form-control', {
               type: 'text',
@@ -121,7 +94,7 @@ const AddRice = {
                 vnode.state.harga = formatHargaInput(value);
               }),
               value: vnode.state.harga
-            })),
+            }))]),
 
              layout.row([
                _formGroup('Garis Lintang', m('input.form-control', {
@@ -146,38 +119,6 @@ const AddRice = {
                }))
              ]),
 
-             m('.reporters.form-group',
-               m('label', 'Otorisasi Reporters'),
-
-               vnode.state.reporters.map((reporter, i) =>
-                 m('.row.mb-2',
-                   m('.col-sm-8',
-                     m('input.form-control', {
-                       type: 'text',
-                       placeholder: 'Tambahkan Reporter berdasarkan nama atau kunci publik...',
-                       oninput: m.withAttr('value', (value) => {
-                         // clear any previously matched values
-                         vnode.state.reporters[i].reporterKey = null
-                         const reporter = vnode.state.agents.find(agent => {
-                           return agent.name === value || agent.key === value
-                         })
-                         if (reporter) {
-                           vnode.state.reporters[i].reporterKey = reporter.key
-                         }
-                       }),
-                       onblur: () => _updateReporters(vnode, i)
-                     })),
-
-                   m('.col-sm-4',
-                     m(MultiSelect, {
-                       label: 'Pilih izin',
-                       options: authorizableProperties,
-                       selected: reporter.properties,
-                       onchange: (selection) => {
-                         vnode.state.reporters[i].properties = selection
-                       }
-                     }))))),
-
              m('.row.justify-content-end.align-items-end',
                m('col-2',
                  m('button.btn.btn-primary',
@@ -191,53 +132,14 @@ const formatHargaInput = (value) => {
 };
 
 /**
- * Update the reporter's values after a change occurs in the name of the
- * reporter at the given reporterIndex. If it is empty, and not the only
- * reporter in the list, remove it.  If it is not empty and the last item
- * in the list, add a new, empty reporter to the end of the list.
- */
-const _updateReporters = (vnode, reporterIndex) => {
-  let reporterInfo = vnode.state.reporters[reporterIndex]
-  let lastIdx = vnode.state.reporters.length - 1
-  if (!reporterInfo.reporterKey && reporterIndex !== lastIdx) {
-    vnode.state.reporters.splice(reporterIndex, 1)
-  } else if (reporterInfo.reporterKey && reporterIndex === lastIdx) {
-    vnode.state.reporters.push({
-      reporterKey: '',
-      properties: []
-    })
-  }
-}
-
-/**
  * Handle the form submission.
  *
  * Extract the appropriate values to pass to the create record transaction.
  */
 const _handleSubmit = (signingKey, state) => {
 
-  // Mengonversi 'DD-MM-YYYY HH:mm' ke format 'YYYY-MM-DDTHH:mm'
-  const parts = state.tgltransaksi.split(" ")
-  const dateParts = parts[0].split("-")
-  const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}T${parts[1]}`
-
-  // Konversi string tanggal yang sudah diformat ke timestamp Tanggal Produksi
-  const tgltransaksiTimestamp = new Date(formattedDate).getTime()
-  // Pastikan hasilnya adalah angka yang valid
-  if (isNaN(tgltransaksiTimestamp)) {
-    alert("Format tanggal tidak valid. Gunakan format DD-MM-YYYY HH:mm")
-    return
-  }
-
-  // Konversi string tanggal yang sudah diformat ke objek Date untuk menghitung Kedaluwarsa
-  const tgltransaksiDate = new Date(formattedDate)
-  // Pastikan hasilnya adalah tanggal yang valid
-  if (isNaN(tgltransaksiDate.getTime())) {
-    alert("Format tanggal produksi tidak valid. Gunakan format DD-MM-YYYY HH:mm")
-    return
-  }
-  // Hitung tanggal kedaluwarsa (2 tahun setelah tgltransaksi)
-  const kedaluwarsaDate = new Date(tgltransaksiDate)
+  // set kedaluwarsaDate to 2 years after today
+  const kedaluwarsaDate = new Date()
   kedaluwarsaDate.setFullYear(kedaluwarsaDate.getFullYear() + 2)
   // Konversi tanggal kedaluwarsa ke timestamp atau format yang diinginkan
   const kedaluwarsaTimestamp = kedaluwarsaDate.getTime()
@@ -252,11 +154,6 @@ const _handleSubmit = (signingKey, state) => {
         name: 'varietas',
         stringValue: state.varietas,
         dataType: payloads.createRecord.enum.STRING
-      },
-      {
-        name: 'tgltransaksi',
-        intValue: tgltransaksiTimestamp,
-        dataType: payloads.createRecord.enum.INT
       },
       {
         name: 'kedaluwarsa',
@@ -283,18 +180,6 @@ const _handleSubmit = (signingKey, state) => {
       }
     ]
   })
-
-  const reporterPayloads = state.reporters
-    .filter((reporter) => !!reporter.reporterKey)
-    .map((reporter) => payloads.createProposal({
-      recordId: state.serialNumber,
-      receivingAgent: reporter.reporterKey,
-      role: payloads.createProposal.enum.REPORTER,
-      properties: reporter.properties
-    }))
-
-  transactions.submit([recordPayload].concat(reporterPayloads), true)
-    .then(() => m.route.set(`/rice/${state.serialNumber}`))
 }
 
 /**
